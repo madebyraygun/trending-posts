1b97d905c4df3452a4398fdef30f26b20221cd23
Dalton Rooney <dalton@madebyraygun.com>
2018-03-31 16:13:24 -0700

    Comment in settings file

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -7,6 +7,6 @@ use craft\base\Model;
 class Settings extends Model
 {
     public $trackDays = 7;
-    public $trackSection = null;
+    public $trackSection = null; //Empty by default means track all sections
     public $visitorTimeout = 15;
 }
